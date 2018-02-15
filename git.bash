83a63fc2c6564b5e01ffd7702b3ae716c4c659ab
Marco Arruda <marco.nc.arruda@gmail.com>
2018-02-15 17:52:47 -0200

    fix

--- git.bash
+++ git.bash
@@ -1,3 +1,21 @@
 function git_status {
-        clear; printf '=%.0s' {1..50}; echo; git remote -v; printf '=%.0s' {1..50}; echo; git branch; printf '=%.0s' {1..50};echo; git status;printf '=%.0s' {1..50};echo; git --no-pager log --pretty=format:"%C(yellow)%h, %Cblue%cn, %C(green)%ar %Creset %s" --graph --all -10;echo;printf '=%.0s' {1..50};echo;git submodule;printf '=%.0s' {1..50};echo;
+        clear;
+        printf '=%.0s' {1..50};
+        echo;
+        git remote -v;
+        printf '=%.0s' {1..50};
+        echo;
+        git branch;
+        printf '=%.0s' {1..50};
+        echo;
+        git status;
+        printf '=%.0s' {1..50};
+        echo;
+        git --no-pager log --pretty=format:"%C(yellow)%h, %Cblue%cn, %C(green)%ar %Creset %s" --graph -10;
+        echo;
+        printf '=%.0s' {1..50};
+        echo;
+        git submodule;
+        printf '=%.0s' {1..50};
+        echo;
 }
